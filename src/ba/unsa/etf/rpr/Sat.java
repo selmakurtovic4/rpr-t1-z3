41e4049f47ea8759ec0dae31754b907781c80b95
student <skurtovic1@etf.unsa.ba>
2021-10-21 17:38:28 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Sat.java
@@ -0,0 +1,36 @@
+package ba.unsa.etf.rpr;
+
+public class Sat {
+    int sati=0, minute=0, sekunde=0;
+    public Sat(int sati, int minute, int sekunde){
+        Postavi(sati, minute, sekunde);
+    }
+    public  void Postavi(int sati,int minute,int sekunde){
+        this.sati=sati;
+        this.minute=minute;
+        this. sekunde=sekunde;    }
+    public  void Sljedeci(){
+        sekunde++;
+        if(sekunde==60){ sekunde=0; minute++;}
+        if(minute==60){ minute=0; sati++;}
+        if(sati==24) sati=0;
+
+    }
+    void Prethodni(){
+        sekunde--;
+        if(sekunde==-1){ sekunde=59; minute--;}
+        if(minute==-1){ minute=59; sati--;}
+        if(sati==-1) sati=23;
+    }
+    void PomjeriZa(int pomak){
+        if(pomak>0) for(int i=0; i<pomak; i++) Sljedeci();
+        else for(int i=0; i<-pomak; i++) Prethodni();
+
+    }
+    int DajSate()  { return sati;}
+    int DajMinute(){ return minute;}
+    int DajSekunde(){ return sekunde;}
+    void Ispisi(){
+        System.out.println(sati+": "+minute+": "+sekunde+"\n");
+    }
+}
